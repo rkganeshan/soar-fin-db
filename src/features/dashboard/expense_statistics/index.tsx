2517bf16a4b6854296332cab3c4940510a2ebe4f
rkganeshan <rkganeshan2010@gmail.com>
2024-12-28 14:08:10 +0530

    Added API expense statistics data

--- src/features/dashboard/expense_statistics/index.tsx
+++ src/features/dashboard/expense_statistics/index.tsx
@@ -1,51 +1,27 @@
 import React from "react";
 import { Pie } from "react-chartjs-2";
+import { useDashboardContext } from "../../../context/dashboardContext";
+import Spinner from "../../../ui/Spinner";
+import { expenseStatisticsPieChartUtils } from "../../../utils/expenseStatisticsPieChart";
+import "./ExpenseStatistics.scss";
 
 const ExpenseStatistics: React.FC = () => {
-  const data = {
-    labels: ["Entertainment", "Investment", "Bill Expense", "Others"],
-    datasets: [
-      {
-        data: [30, 20, 35, 15],
-        backgroundColor: ["#FC7900", "#396AFF", "#343C6A", "#232323"],
-        borderColor: "#fff",
-        borderWidth: 2,
-        hoverOffset: 20,
-        offset: [5, 40, 5, 30],
-      },
-    ],
-  };
+  const {
+    isLoadingDashboard,
+    isErrorDashboard,
+    statistics: expenseStatsData,
+  } = useDashboardContext();
+  const { pieChartData, pieChartOptions } = expenseStatisticsPieChartUtils({
+    expenseStatsData,
+  });
 
-  const options: any = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        enabled: false,
-      },
-      datalabels: {
-        color: "#fff",
-        font: {
-          weight: "bold",
-          size: 12,
-        },
-        textAlign: "center",
-        formatter: (value: number, context: any) => {
-          const percentage = `${value}%`;
-          const label = context.chart.data.labels[context.dataIndex];
-          return `${percentage}\n${label}`;
-        },
-        align: ["center", "end", "center", "center"],
-        anchor: "center",
-      },
-    },
-    animation: {
-      animateRotate: true,
-      animateScale: true,
-    },
+  const rendePieChartSection = () => {
+    if (isLoadingDashboard) {
+      return <Spinner expand={false} />;
+    }
+    if (!isErrorDashboard) {
+      return <Pie data={pieChartData} options={pieChartOptions} />;
+    }
   };
 
   return (
@@ -53,17 +29,8 @@ const ExpenseStatistics: React.FC = () => {
       <h2 className="text-lg font-semibold mb-4" style={{ color: "#343C6A" }}>
         Expense Statistics
       </h2>
-      <div
-        className="bg-white rounded-2xl shadow"
-        style={{
-          paddingLeft: "15px",
-          paddingRight: "15px",
-          paddingTop: "16px",
-          paddingBottom: "16px",
-          height: "290px",
-        }}
-      >
-        <Pie data={data} options={options} />
+      <div className="pie-chart-container bg-white rounded-2xl shadow">
+        {rendePieChartSection()}
       </div>
     </div>
   );
